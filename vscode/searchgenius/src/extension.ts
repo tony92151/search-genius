9cd04ee8cdfb17dc2416fa45df3549eba6822df7
tonyguo <tony92151@gmail.com>
2023-11-25 21:51:02 +0800

    add activity bar

--- vscode/searchgenius/src/extension.ts
+++ vscode/searchgenius/src/extension.ts
@@ -2,24 +2,13 @@
 // Import the module and reference it with the alias vscode in your code below
 
 
-import {
-	ConfigurationTarget,
-	InputBoxValidationSeverity,
-	ProgressLocation,
-	Uri,
-	ThemeIcon,
-	ExtensionContext,
-	workspace,
-	window,
-	env,
-	commands,
-  } from "vscode";
+import * as vscode from 'vscode';
 
-const configTarget = ConfigurationTarget.Global;
+const configTarget = vscode.ConfigurationTarget.Global;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -28,19 +17,72 @@ export function activate(context: ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let checkEndpoint = commands.registerCommand('searchgenius.checkEndpoint', () => {
-		const configuration = workspace.getConfiguration("searchgenius");
+	let checkEndpoint = vscode.commands.registerCommand('searchgenius.checkEndpoint', () => {
+		const configuration = vscode.workspace.getConfiguration("searchgenius");
 		let  apiEndpoint = configuration.get("api.endpoint");
-		window.showInformationMessage(`api.endpoint: ${apiEndpoint}`);
+		vscode.window.showInformationMessage(`api.endpoint: ${apiEndpoint}`);
 	});
 
-	let openSettings = commands.registerCommand('searchgenius.openSettings', () => {
-		commands.executeCommand("workbench.action.openSettings", "searchgenius.api.endpoint");
-		window.showInformationMessage('Open Settings');
+	let openSettings = vscode.commands.registerCommand('searchgenius.openSettings', () => {
+		vscode.commands.executeCommand("workbench.action.openSettings", "searchgenius.api.endpoint");
+		vscode.window.showInformationMessage('Open Settings');
 	});
 
-	context.subscriptions.push(checkEndpoint, openSettings);
+	const view = vscode.window.registerWebviewViewProvider('searchgenius.webview', new SearchGeniusViewProvider(), {
+        webviewOptions: {
+            retainContextWhenHidden: true
+        }
+    });
+
+	context.subscriptions.push(checkEndpoint, openSettings, view);
 }
+
+class SearchGeniusViewProvider implements vscode.WebviewViewProvider {
+    private _view?: vscode.WebviewView;
+
+    public resolveWebviewView(
+        webviewView: vscode.WebviewView,
+        context: vscode.WebviewViewResolveContext,
+        _token: vscode.CancellationToken
+    ) {
+        this._view = webviewView;
+
+        webviewView.webview.options = {
+            // Enable scripts in the webview
+            enableScripts: true
+        };
+
+        // Set the HTML content for the webview
+        webviewView.webview.html = this.getHtmlForWebview(webviewView.webview);
+    }
+
+    private getHtmlForWebview(webview: vscode.Webview) {
+        // Return HTML content with a button and a text box
+        return `<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="Content-Security-Policy" content="default-src 'none'; script-src ${webview.cspSource}; style-src ${webview.cspSource};">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                </head>
+                <body>
+                    <button id="myButton">Click Me!</button>
+                    <textarea id="textBox" rows="4" cols="50"></textarea>
+
+                    <script>
+                        const button = document.getElementById('myButton');
+                        const textBox = document.getElementById('textBox');
+
+                        button.addEventListener('click', () => {
+                            const randomText = 'Random Text: ' + Math.random().toString(36).substring(2, 15);
+                            textBox.value += randomText + '\\n';
+                        });
+                    </script>
+                </body>
+                </html>`;
+    }
+}
+
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
